76cffc1f6fa8dfa85a4f0161ad9bdc1f41d58b24
carlossarrion-wq <carlossarrion@gmail.com>
2026-01-12 19:54:01 +0100

    chore: Actualizar configuración de AgentCore
    - Actualizar .bedrock_agentcore.yaml con nueva configuración
    - Actualizar config_agent.py con últimos cambios

--- config_agent.py
+++ config_agent.py
@@ -6,7 +6,7 @@ Actualiza este archivo con los datos de tu agente desplegado.
 """
 
 # ARN completo del agente desplegado en AWS AgentCore
-AGENT_ARN = "arn:aws:bedrock-agentcore:eu-west-1:701055077130:runtime/agente_strands_agentcore-BFgYot85YJ/runtime-endpoint/DEFAULT"
+AGENT_ARN = "arn:aws:bedrock-agentcore:eu-west-1:701055077130:runtime/agente_strands_agentcore-vtcF764r3U/runtime-endpoint/DEFAULT"
 
 # Región de AWS donde está desplegado el agente
 REGION = "eu-west-1"
@@ -15,4 +15,4 @@ REGION = "eu-west-1"
 ACCOUNT_ID = "701055077130"
 
 # Nombre del agente
-AGENT_NAME = "agente_strands_agentcore-BFgYot85YJ"
+AGENT_NAME = "agente_strands_agentcore-vtcF764r3U"
